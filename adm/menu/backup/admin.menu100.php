eaf1c4a68264793a9f5092bda7b0fff1a6709629
smboy86 <smboy86@naver.com>
2018-05-06 19:09:06 +0900

    [추가] 어드민 메뉴 복구

---
+++ adm/menu/backup/admin.menu100.php
@@ -0,0 +1,6 @@
+<?php
+$menu['menu100'] = array (
+    array('100000', '회원리스트', G5_ADMIN_URL.'/config_form.php',   'config'),
+    array('100100', '회원목록', G5_ADMIN_URL.'/config_form.php',   'cf_basic'),
+);
+?>
