abfee1c2e241fecbf7b88aa42e0b10bcc2a5fdf2
smboy86 <smboy86@naver.com>
2018-05-09 22:41:02 +0900

    [삭제] menu 백업 폴더 삭제

--- adm/menu/backup/admin.menu200.php
+++
@@ -1,7 +0,0 @@
-<?php
-$menu['menu200'] = array (
-    array('200000', '어드민관리', G5_ADMIN_URL.'/member_list.php', 'member'),
-    array('200100', '승인 목록', G5_ADMIN_URL.'/member_list.php', 'mb_list'),
-    array('200200', '프로그램 일정 관리(캘린더)', G5_ADMIN_URL.'/mail_list.php', 'mb_mail'),
-);
-?>
